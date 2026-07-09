801a06967b492b9c0171be57da784e3511bc57c1
Avinash Marapatla <m.avinashavi25@gmail.com>
2026-07-09 22:20:27 +0530

    Complete Python Basic Exercises

---
+++ src/01_basics/variables.py
@@ -0,0 +1,37 @@
+# Variables
+
+# Datatypes
+# Primitive Datatype
+# Int | string | Float | Boolean
+
+# Integers
+roll_no = 65
+id = 101
+
+print(roll_no)
+print(id)
+
+#Float
+weight = 72.5
+height = 5.4
+
+print(weight)
+print(height)
+
+
+# string #naming format snake_case
+first_name = "Avinash"
+last_name = "Marapatla" 
+
+print(f"{first_name} {last_name}") #formatted 
+
+full_name = first_name + " " +last_name #adding or concatenating
+print(full_name)
+
+# Boolean  True or false
+is_present = True
+print(is_present)
+
+attendance= bool(input("is he present in classroom: "))
+print(attendance)
+print(type(attendance))
