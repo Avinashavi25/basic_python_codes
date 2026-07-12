631654d6d98fce8774d077db72f2af0273a8fb60
Avinash Marapatla <m.avinashavi25@gmail.com>
2026-07-12 23:44:18 +0530

    python basics: variable, operators, conditional statement and loops

--- src/01_basics/variables.py
+++ src/01_basics/variables.py
@@ -5,33 +5,93 @@
 # Int | string | Float | Boolean
 
 # Integers
-roll_no = 65
-id = 101
+# roll_no = 65
+# id = 101
 
-print(roll_no)
-print(id)
+# print(roll_no)
+# print(id)
 
-#Float
-weight = 72.5
-height = 5.4
+# #Float
+# weight = 72.5
+# height = 5.4
 
-print(weight)
-print(height)
+# print(weight)
+# print(height)
 
 
-# string #naming format snake_case
-first_name = "Avinash"
-last_name = "Marapatla" 
+# # string #naming format snake_case
+# first_name = "Avinash"
+# last_name = "Marapatla" 
 
-print(f"{first_name} {last_name}") #formatted 
+# print(f"{first_name} {last_name}") #formatted 
 
-full_name = first_name + " " +last_name #adding or concatenating
-print(full_name)
+# full_name = first_name + " " +last_name #adding or concatenating
+# print(full_name)
 
 # Boolean  True or false
-is_present = True
-print(is_present)
+# is_present = True
+# print(is_present)
 
-attendance= bool(input("is he present in classroom: "))
-print(attendance)
-print(type(attendance))
+# attendance= bool(input("is he present in classroom: "))
+# print(attendance)
+# print(type(attendance))
+
+# Typecasting
+# Age = int(input("What is your age:"))
+# Weight = float(input("What is your weight:"))
+# Name = input("What is your name: ")
+
+# print(f"I am {Name} and my age is {Age} and weight {weight} ")
+# ceil and floor to use we need library Math
+# Flr_value = floor(2.5)
+
+# Operators
+# +, -, /, //, *
+a = 10
+# b = 15
+
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a//b)
+# print(a*b)
+# x=9
+# # Assignment operator
+# x+=a
+# print(x)
+
+# Membership operator in
+# text = 'python'
+# print('P' in text)
+# print()
+# # identity operator is: it compares the object; == compares value
+# a= [1,2]
+# b= a
+# c = [2,1]
+# d=c
+# print(a is b)
+# # print(d is c)
+# print(d == b)
+
+# Conditional Statement
+# a = int(input())
+# b=int(input())
+# if a > b:
+#     print("a is larger than b")
+# else: 
+#     print("b is larger than a")
+
+# # loops
+# first_line = [34,23,1,76,90]
+# for i in first_line:
+#     print(i)
+# *
+# **
+# ***
+# ****
+# *****
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*",end="")
+#     print("\n")
